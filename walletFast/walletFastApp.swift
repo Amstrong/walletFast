12f831b3ab4c910c307191065c061d26490e118f
Amstrong <monachelloa@uninorte.edu.co>
2022-12-03 18:05:45 +0100

    Initial Commit

---
+++ walletFast/walletFastApp.swift
@@ -0,0 +1,17 @@
+//
+//  walletFastApp.swift
+//  walletFast
+//
+//  Created by Amstrong Monachello on 3/12/22.
+//
+
+import SwiftUI
+
+@main
+struct walletFastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
